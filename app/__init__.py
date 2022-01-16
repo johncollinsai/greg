708b4538e167c2804115e1f4590e0bb8d003c851
john collins <john.collins@edhec.com>
2022-01-16 20:10:57 +0800

    v0.10.0, implemented bootstrap3

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@
 migration engine instances. These are created after the application. 
 """
 
-from flask import Flask    # Flask (upper case) class imported from flask package
-from config import Config  # Config class (upper case) imported from config.py
+from flask import Flask    # Flask (upper case) class imported from flask package (lower case)
+from config import Config  # Config class (upper case) imported from config.py (lower case)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 ''' app.config.from_object method tells Flask to read and apply the Config class,
@@ -13,5 +14,6 @@ which it finds in config.py'''
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
