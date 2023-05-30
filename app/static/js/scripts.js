494b91a181afb236e56bf015322b653bb7692161
johncollinsai <climatemetrics@gmail.com>
2023-05-31 05:53:31 +0800

    v0.5.20, activating modal for images and results table

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -5,13 +5,19 @@ if (typeof window !== 'undefined') {
   jQuery = require('jquery');
 }
 
-
-// Opens modal when user clicks on a row in the table - NOT WORKING YET, SEE CHATGPT "SHOW IMAGES"
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal
 var modalImg = document.getElementById("img01");
+
+// Create a new span element for the close button
+var span = document.createElement("span");
+span.className = "close";
+span.innerHTML = "&times;";
+
+// Append the close button to the modal
+modal.appendChild(span);
 
 // Get the table rows
 var rows = document.querySelectorAll(".table-hover tr");
@@ -31,13 +37,11 @@ rows.forEach(function(row) {
   });
 });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
 
 // Create Chart.js chart
 var Chart = window.Chart; // Get the chart class from the window object
